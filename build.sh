beffca8bded29a3fa2ec30a880b8f5b6e8fda75e
Afn273 <i200908@nu.edu.pk>
2024-06-10 19:32:30 +0500

    d

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # Build the project
 echo "Building the project..."
-python311 -m pip install -r requirements.txt
-echo "Make Migration..."
-python311 manage.py makemigrations --noinput
-python311 manage.py migrate --noinput
+python -m pip install -r requirements.txt
+# echo "Make Migration..."
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
 echo "Collect Static..."
-python311 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
